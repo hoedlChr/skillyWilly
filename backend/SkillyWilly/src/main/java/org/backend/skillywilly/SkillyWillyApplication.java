da33ce4e89355aa21f7acd92c3a4d9f67cd81192
Markus Kummer <52301628@edu.campus02.at>
2025-03-14 19:20:11 +0100

    initial commit

---
+++ backend/SkillyWilly/src/main/java/org/backend/skillywilly/SkillyWillyApplication.java
@@ -0,0 +1,13 @@
+package org.backend.skillywilly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillyWillyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillyWillyApplication.class, args);
+	}
+
+}
