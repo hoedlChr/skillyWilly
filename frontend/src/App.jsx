d061d2b4c1aca65b197d7169ac27e387f7a3fdb5
Christoph Hoedl <christophhodl@gmail.com>
2025-03-16 18:37:59 +0100

    ErrorBoundary hinzugefügt

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,17 +2,20 @@ import { useState } from 'react'
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Test from "./pages/Test";
 import Welcome from './pages/Welcome';
+import ErrorBoundary from "./ErrorBoundary";
 
 function App() {
 
   return (
     <>
+    <ErrorBoundary>
       <BrowserRouter>
         <Routes path='/'>
           <Route index element={<Welcome/>}/>
           <Route path="test" element={<Test/>}/>
         </Routes>
       </BrowserRouter>
+    </ErrorBoundary>
     </>
   )
 }
