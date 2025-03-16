93a529787ca766d16531ab5b6b7bf241a665b7f0
Christoph Hoedl <christophhodl@gmail.com>
2025-03-16 17:49:35 +0100

    React Router implementiert und test seiten hinzugefügt

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,36 +1,21 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Test from "./pages/Test";
+import Welcome from './pages/Welcome';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-    <button className='btn btn-danger'>hallo</button>
-    <i class="fa fa-car"></i>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <BrowserRouter>
+        <Routes path='/'>
+          <Route index element={<Welcome/>}/>
+          <Route path="test" element={<Test/>}/>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
+
 
 export default App
---
+++ frontend/src/pages/Welcome.jsx
@@ -0,0 +1,18 @@
+import React, { useEffect } from 'react';
+
+function Welcome() {
+
+    useEffect(()=>{
+
+    },[])
+
+
+    return (
+        <div className='container'>
+            <h1>Welcome to SkillyWilly!</h1>
+            <p>Your journey to mastering new skills starts here.</p>
+        </div>
+    );
+}
+
+export default Welcome;
