bdd7a514575a14d1f1dab2ee90ad11f5eba4e826
Christoph Hoedl <christophhodl@gmail.com>
2025-05-03 11:54:58 +0200

    Skills bearbeiten

--- frontend/src/pages/MySkilly.jsx
+++ frontend/src/pages/MySkilly.jsx
@@ -1,42 +1,53 @@
 import React, { useEffect, useState } from 'react';
-import StandardInputField from '../components/StandardInputField';
-import Button from '../components/Button';
-import Chat from './dashboard/Chat';
-import ChatList from './dashboard/ChatList';
 import ElementList from './dashboard/ElementList';
-import Filter from './dashboard/Filter';
-import ElementView from './dashboard/ElementView';
 import CreateSkill from './dashboard/CreateSkill';
-import SkillOnMap from './dashboard/SkillOnMap';
 import Navbar from './dashboard/Navbar';
+import ElementEditor from './dashboard/ElementEditor';
 
-function MySkilly({user, setUser}) {
-    const [showChat, setShowChat] = useState(false);
+function MySkilly({user}) {
     const [showElement, setShowElement] = useState(false);
     const [showCreateSkill, setShowCreateSkill] = useState(false);
-    const [showElementsOnMap, setShowElementsOnMap] = useState(false);
-
+    const [data, setData] = useState([]);
+    const [allUserData, setAllUserData] = useState({});
     const height = 90;
     useEffect(()=>{
-
+        //fetch user data
+        fetch(`/api/users/${user.id}`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        })
+        .then((res) => {
+            if (!res.ok) {
+                throw new Error(`HTTP error! status: ${res.status}`);
+            }
+            return res.json();
+        })
+        .then((data) => {
+            setData(data.skills);
+            setAllUserData(data);
+        })
+        .catch((error) => {
+            console.error("Error fetching user data:", error);
+        });
     },[])
 
-
+    const users = {[user.id]: allUserData};
     return (<>
-        <CreateSkill show={showCreateSkill} setShow={setShowCreateSkill}/>
-        <SkillOnMap show={showElementsOnMap} setShow={setShowElementsOnMap}/>
+        <CreateSkill show={showCreateSkill} user={user} setShow={setShowCreateSkill}/>
         <div className='container'>
-            <Navbar text="MySkilly" setShowCreateSkill={setShowCreateSkill} setShowElementsOnMap={setShowElementsOnMap}/>
+            <Navbar text="MySkilly" setShowCreateSkill={setShowCreateSkill}/>
             <div className='row'>
                 <div className='col-5 overflow-auto' style={{height: `${height}vh`}}>
-                        <ElementList mySkilly={true} setShowChat={setShowChat} setShowElement={setShowElement}/>
+                        <ElementList data={data} users={users} mySkilly={true} setShowElement={setShowElement}/>
                 </div>
                 {
                     showElement === false ? null:
                     <div className="col-4" style={{height: `${height}vh`}}>
                         {
                             showElement ?
-                            <ElementView/> :null
+                            <ElementEditor id={showElement} data={data} setShowElement={setShowElement} user={user}/> :null
                         }
                     </div>
                 }
