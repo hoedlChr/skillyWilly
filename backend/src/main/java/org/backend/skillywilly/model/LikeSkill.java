d3f7b1985aac4597afdbca445d33aabf22741a65
Markus Kummer <52301628@edu.campus02.at>
2025-05-06 14:38:34 +0200

    optimierung

--- backend/src/main/java/org/backend/skillywilly/model/LikeSkill.java
+++ backend/src/main/java/org/backend/skillywilly/model/LikeSkill.java
@@ -1,9 +1,7 @@
 package org.backend.skillywilly.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Data;
-import lombok.ToString;
 
 /**
  * Represents the "LikeSkill" entity, which establishes a link between a user and a skill they have "liked".
@@ -14,7 +12,7 @@ import lombok.ToString;
  * It contains references to both the user who liked the skill and the skill being liked.
  */
 @Entity
-@Table(name = "LikeSkill")
+@Table(name = "like_skill")
 @Data
 public class LikeSkill {
 
@@ -45,31 +43,4 @@ public class LikeSkill {
      */
     @Column(name = "skill_id", nullable = false)
     private Long skillId;
-
-    /**
-     * Represents the association between the LikeSkill entity and the User entity.
-     * This is a many-to-one relationship where a LikeSkill references a single User.
-     * The user is identified by the foreign key column "user_id".
-     * Changes to this field are not directly insertable or updatable, as it is used
-     * for mapping purposes to the associated User entity.
-     */
-    @ManyToOne
-    @JoinColumn(name = "user_id", insertable = false, updatable = false)
-    @JsonIgnore
-    @ToString.Exclude
-    private User user;
-    ;
-
-    /**
-     * Represents the many-to-one relationship between the LikeSkill entity and the Skill entity.
-     * This variable maps to the "skill_id" column in the LikeSkill table and references the Skill entity.
-     * It is marked as non-insertable and non-updatable to ensure that the value is managed only through
-     * the relationship with the corresponding Skill entity.
-     */
-    @ManyToOne
-    @JoinColumn(name = "skill_id", insertable = false, updatable = false)
-    @JsonIgnore
-    @ToString.Exclude
-    private Skill skill;
-
 }
