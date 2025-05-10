2911e3d6e76c6906468411833f5fd5f6cea4a986
Markus Kummer <52301628@edu.campus02.at>
2025-05-10 10:45:09 +0200

    token

--- backend/src/main/java/org/backend/skillywilly/repository/MessageRepository.java
+++ backend/src/main/java/org/backend/skillywilly/repository/MessageRepository.java
@@ -2,7 +2,11 @@ package org.backend.skillywilly.repository;
 
 import org.backend.skillywilly.model.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * Repository interface for managing {@link Message} entities.
@@ -13,4 +17,16 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface MessageRepository extends JpaRepository<Message, Long> {
+
+    /**
+     * Finds all messages that a user has sent or received.
+     *
+     * @param userId ID of the user
+     * @return List of messages where the user is either sender or recipient,
+     * ordered by creation timestamp in descending order
+     */
+    @Query("SELECT m FROM Message m WHERE m.userFromId = :userId OR m.userToId = :userId " +
+            "ORDER BY m.created DESC")
+    List<Message> findAllUserMessages(@Param("userId") Long userId);
 }
+
