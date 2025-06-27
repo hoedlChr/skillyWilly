a1d1b94e27ba1634a68f4d8081cd147ccd0499c6
Markus Kummer <52301628@edu.campus02.at>
2025-06-27 10:39:23 +0200

    unfollow fix

--- backend/src/main/java/org/backend/skillywilly/service/FollowerService.java
+++ backend/src/main/java/org/backend/skillywilly/service/FollowerService.java
@@ -4,6 +4,7 @@ import org.backend.skillywilly.model.Follower;
 import org.backend.skillywilly.repository.FollowerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -17,6 +18,7 @@ public class FollowerService {
         return followerRepository.save(follower);
     }
 
+    @Transactional
     public void unfollowUser(Long followerId, Long followingId) {
         followerRepository.deleteByUserFollowerIdAndUserFollowedId(followerId, followingId);
     }
