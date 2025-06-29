5319858998aa16a2b57825e1c423bc58e5f3831a
Markus Kummer <52301628@edu.campus02.at>
2025-06-29 19:54:48 +0200

    reset-password

--- backend/src/main/java/org/backend/skillywilly/service/UserService.java
+++ backend/src/main/java/org/backend/skillywilly/service/UserService.java
@@ -73,4 +73,12 @@ public class UserService {
         return userRepository.save(user);
     }
 
+    public Optional<User> findByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+
 }
